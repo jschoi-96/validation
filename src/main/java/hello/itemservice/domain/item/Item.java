ffe67055520e8776abeac57878299e13367ca06b
junsoo <choix712@umn.edu>
2023-09-06 00:47:56 +0900

    Groups 기능을 적용해서 Add / Edit 각 검증을 다르게 구현

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -13,18 +13,18 @@ import javax.validation.constraints.NotNull;
 // ObjectError -> 권장하지 않음
 public class Item {
 
-    @NotNull // 수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class , UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class , UpdateCheck.class})
+    @Range(min = 1000, max = 1000000 , groups = {SaveCheck.class , UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    // @Max(9999) // 수정 요구사항 추가
+    @NotNull(groups = {SaveCheck.class , UpdateCheck.class})
+    @Max(value = 9999 , groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
